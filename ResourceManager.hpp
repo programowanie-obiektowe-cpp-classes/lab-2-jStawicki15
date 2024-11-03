0637b621ac5a7490c0fff7bc1f6b926542ed2b23
Jakub Stawicki <jakub.stawicki15@gmail.com>
2024-11-03 14:57:58 +0100

    Pierwszy test kodu

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,30 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+    Resource* resource_;
+    
+    public:
+    double get()
+    {
+        return resource_->get();
+    }
+
+    //Metody Specjalne
+    //Konstruktor
+    ResourceManager()
+    {
+        resource_ = new Resource();
+    }
+    //Destruktor
+    ~ResourceManager()
+    {
+        delete resource_;
+    }
+    //Kopiowanie
+    ResourceManager& operator=(ResourceManager& oryginal)
+    {
+        this->resource_ = oryginal.resource_;
+    }
+    //Przenoszenie
 };
